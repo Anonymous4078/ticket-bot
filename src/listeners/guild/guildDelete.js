8a2579857172fbc0350730f78009bb5ab2e38151
Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>
2022-06-28 23:18:31 +0530

    Add files via upload

---
+++ src/listeners/guild/guildDelete.js
@@ -0,0 +1,43 @@
+const {
+  bold,
+  EmbedBuilder,
+  underscore,
+  time,
+  TimeStampStyles,
+} = require('discord.js');
+
+module.exports = {
+  name: 'guildDelete',
+  execute: async (client, guild) => {
+    const owner = await guild.fetchOwner();
+
+    const embed = new EmbedBuilder()
+      .setTitle('Joined a new server')
+      .setThumbnail(guild.iconURL())
+      .setTimestamp()
+      .addFields([
+        {
+          name: `${underscore('Server Information :')}`,
+          value: `
+${bold('Name:')} ${guild.name} 
+${bold('ID:')} ${guild.id} 
+${bold('Members:')} ${guild.memberCount}
+${bold('Created At:')} ${time(
+            guild.createdTimestamp,
+            TimeStampStyles.LongDateTime,
+          )} ( ${time(guild.createdTimestamp, TimeStampStyles.RelativeTime)} )`,
+          inline: true,
+        },
+        {
+          name: `${underscore('Owner Information :')}`,
+          value: `
+${bold('Owner:')} ${owner.user.tag} 
+${bold('Owner ID:')} ${owner.id}
+`,
+          inline: true,
+        },
+      ]);
+    const channel = client.channels.cache.get(client.config.logChannelId);
+    channel.send({ embeds: [embed] });
+  },
+};
--- src/listeners/guild/interactionCreate.js
+++ src/listeners/guild/interactionCreate.js
@@ -1,1 +1,67 @@
+const prettyMilliseconds = require('pretty-ms');
+const { Collection } = require('discord.js');
 
+module.exports = {
+  name: 'interactionCreate',
+  execute: async (client, interaction) => {
+    const { user } = interaction;
+    if (interaction.isButton()) {
+      const component = client.interactions.get(interaction.customId);
+      if (!component) return;
+
+      component.interactionRun(interaction);
+    } else if (interaction.isChatInputCommand()) {
+      const command = client.commands.get(interaction.commandName);
+
+      if (!command) return;
+
+      if (
+        !interaction.member.permissions.has(command.requiredUserPermissions)
+      ) {
+        return interaction.reply(
+          `${client.config.emojis.cross} | You don't have permissons to use this command.`,
+        );
+      }
+
+      const { cooldowns } = client;
+
+      if (!cooldowns.has(command.data.name)) {
+        cooldowns.set(command.data.name, new Collection());
+      }
+
+      const now = Date.now();
+      const timestamps = cooldowns.get(command.data.name);
+      const cooldownAmount = command.cooldown * 1000;
+
+      if (timestamps.has(user.id)) {
+        const expirationTime = timestamps.get(user.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+          const timeLeft = (expirationTime - now) / 1000;
+          return interaction.reply(
+            `🕐 | Please wait ${prettyMilliseconds(timeLeft, {
+              verbose: true,
+            })} before reusing this command.`,
+          );
+        }
+      }
+
+      timestamps.set(user.id, now);
+      setTimeout(() => timestamps.delete(user.id), cooldownAmount);
+
+      command.chatInputRun(interaction).catch((err) => {
+        client.logger.error(err);
+        if (interaction.replied) {
+          interaction.editReply(
+            '⚠️ | An error occurred while executing this command.',
+          );
+        } else {
+          interaction.reply({
+            content: '⚠️ | An error occurred while executing this command.',
+            ephemeral: true,
+          });
+        }
+      });
+    }
+  },
+};
