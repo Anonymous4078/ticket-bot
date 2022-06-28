14cfc24cb591ded19088ca6e6f688d0a4dfd4432
Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>
2022-06-28 23:13:46 +0530

    Add files via upload

---
+++ src/commands/info/invite.js
@@ -0,0 +1,38 @@
+const {
+  EmbedBuilder,
+  hyperlink,
+  PermissionFlagsBits,
+  OAuth2Scopes,
+} = require('discord.js');
+
+module.exports = {
+  data: {
+    name: 'invite',
+    description: 'Invite the bot.',
+  },
+  chatInputRun: async (interaction) => {
+    await interaction.deferReply({ ephemeral: true });
+
+    const { client } = interaction;
+    const invite = client.generateInvite({
+      scopes: [OAuth2Scopes.ApplicationsCommands, OAuth2Scopes.Bot],
+      permissions: [
+        PermissionFlagsBits.EmbedLinks,
+        PermissionFlagsBits.SendMessages,
+        PermissionFlagsBits.ManageChannels,
+        PermissionFlagsBits.ReadMessageHistory,
+        PermissionFlagsBits.ViewChannel,
+      ],
+    });
+
+    const embed = new EmbedBuilder()
+      .setTitle(`Invite ${client.user.username}`)
+      .setDescription(
+        `• ${hyperlink('Click here', invite)} to add me to any of your servers. \n• Join ${hyperlink('Suppirt server', 'https://discord.gg/5n3pb4PwFN')} if you need support with the bot.`,
+      )
+      .setColor('Blurple')
+      .setTimestamp();
+
+    await interaction.editReply({ embeds: [embed] });
+  },
+};
--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,1 +1,23 @@
+const { inlineCode } = require('discord.js');
 
+module.exports = {
+  data: {
+    name: 'ping',
+    description: 'Shows the ping of the bot.',
+  },
+  chatInputRun: async (interaction) => {
+    await interaction.deferReply();
+    const sent = await interaction.editReply({
+      content: '🏓 | Pinging...',
+      fetchReply: true,
+    });
+
+    await interaction.editReply(
+      `🏓 | Pong! Websocket heartbeat : ${inlineCode(
+        interaction.client.ws.ping,
+      )}ms, Roundtrip latency : ${inlineCode(
+        sent.createdTimestamp - interaction.createdTimestamp,
+      )}ms.`,
+    );
+  },
+};
